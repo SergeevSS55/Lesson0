c9178d83a322b6f50af71a24a9e202c74d8a286a
SergeevSS55 <yourhonor55@mail.ru>
2024-06-05 20:02:58 +0300

    Add files via upload

---
+++ Индексация строк.py
@@ -0,0 +1,10 @@
+name = 'это моя строка'
+print(name[0])
+print(name[-1])
+print(name[3:5])
+print(name[::-1])
+print(len(name))
+print('это новая строка ' + name)
+
+name1 = 'это новая строка'
+print(name1 + ' ' + name)
