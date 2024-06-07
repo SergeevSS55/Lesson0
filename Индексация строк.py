1522f01aa69d4466ad217996800c10e21576d07e
SergeevSS55 <yourhonor55@mail.ru>
2024-06-07 15:20:03 +0300

    Add files via upload

--- Индексация строк.py
+++ Индексация строк.py
@@ -1,10 +1,7 @@
-name = 'это моя строка'
-print(name[0])
-print(name[-1])
-print(name[3:5])
-print(name[::-1])
-print(len(name))
-print('это новая строка ' + name)
-
-name1 = 'это новая строка'
-print(name1 + ' ' + name)
+example =  'Топинамбур'
+print(len(example))
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1:10:2])
