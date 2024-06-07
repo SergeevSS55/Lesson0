442e74c804c9c8548e9a3bead4c6c9123b3404e6
SergeevSS55 <yourhonor55@mail.ru>
2024-06-07 15:42:05 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+amount_of_homework = 12
+hours = 1.5
+title = 'Phyton'
+time_for_task = hours / amount_of_homework
+print('Курс: ' + title + ',', 'всего задач:'
+      + str(amount_of_homework), 'затрачено часов: ' + str(hours) + ',',
+      'среднее время выполнения ' + str(time_for_task) + ' часа.')
